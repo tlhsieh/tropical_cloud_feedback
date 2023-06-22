4bfef19190e0153d1b354645a0fe84a17c4acc00
Tsung-Lin Hsieh <hsiehtl@princeton.edu>
2023-06-22 17:02:04 -0400

    update year range

--- load.py
+++ load.py
@@ -1,21 +1,19 @@
 import numpy as np
 import xarray as xr
 
-from param import model_list
-
 def load_cmip(model, field, exp='piControl'):
-    return xr.open_dataset(f'/tigress/cw55/data/CMIP6_post/CMIP6_post_regrid/{exp}/{model}/{field}/{field}.mon.0001-0200.nc.r1i1p1f1.1x1.20210826')
+    if exp == 'piControl':
+        yrend = '200'
+    elif exp == 'abrupt-4xCO2':
+        yrend = '150'
+    else:
+        yrend = '200'
+
+    file_path = f'/tigress/cw55/data/CMIP6_post/CMIP6_post_regrid/{exp}/{model}/{field}/{field}.mon.0001-0{yrend}.nc.r1i1p1f1.1x1.20210826'
+
+    # print(file_path)
+
+    return xr.open_dataset(file_path)
 
 def load_feedback(model):
     return xr.open_dataset(f'/tigress/cw55/data/CMIP6_post/abrupt-4xCO2/{model}/rk/rk.GFDL.toa.0001-0150.nc.r1i1p1f1.2x2.5.2021052021')
-
-if __name__ == '__main__':
-    for model in model_list():
-        print(model)
-
-        ds = load_cmip(model, 'ua')
-        ds = load_cmip(model, 'va')
-        ds = load_cmip(model, 'wap')
-
-        ds = load_feedback(model)
-        print(ds)
